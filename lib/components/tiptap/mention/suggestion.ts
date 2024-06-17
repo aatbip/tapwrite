ca405ca46bfe4fa98787f9ace0ac12b6543b1850
Arpan Dhakal <arpandhakal20@gmail.com>
2024-06-17 18:40:53 +0545

    feat : mentions configuration changed and added new suggestions prop

---
+++ lib/components/tiptap/mention/suggestion.ts
@@ -0,0 +1,61 @@
+import { ReactRenderer } from "@tiptap/react";
+import tippy from "tippy.js";
+
+import MentionList from "./MentionList.tsx";
+
+export default {
+  render: () => {
+    let component: any;
+    let popup: any;
+
+    return {
+      onStart: (props: any) => {
+        component = new ReactRenderer(MentionList, {
+          props,
+          editor: props.editor,
+        });
+
+        if (!props.clientRect) {
+          return;
+        }
+
+        popup = tippy("body", {
+          getReferenceClientRect: props.clientRect,
+          appendTo: () => document.body,
+          content: component.element,
+          showOnCreate: true,
+          interactive: true,
+          trigger: "manual",
+          placement: "bottom-start",
+        });
+      },
+
+      onUpdate(props: any) {
+        component.updateProps(props);
+
+        if (!props.clientRect) {
+          return;
+        }
+
+        popup[0].setProps({
+          getReferenceClientRect: props.clientRect,
+        });
+      },
+
+      onKeyDown(props: any) {
+        if (props.event.key === "Escape") {
+          popup[0].hide();
+
+          return true;
+        }
+
+        return component.ref?.onKeyDown(props);
+      },
+
+      onExit() {
+        popup[0].destroy();
+        component.destroy();
+      },
+    };
+  },
+};
