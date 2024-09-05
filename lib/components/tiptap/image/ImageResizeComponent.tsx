df1c77e6c6c22c28be9b7540b07b4873ce515bb2
Arpan Dhakal <arpandhakal20@gmail.com>
2024-09-05 17:59:02 +0545

    update : version

--- lib/components/tiptap/image/ImageResizeComponent.tsx
+++ lib/components/tiptap/image/ImageResizeComponent.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { NodeViewWrapper } from '@tiptap/react'
 import { Resize } from './resizeIcon'
 
