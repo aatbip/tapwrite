a8742c0c27df20ae397a4ba5996f8c6f293c1e98
Arpan Dhakal <arpandhakal20@gmail.com>
2024-09-26 14:41:44 +0545

    fix : image resize component renewed

--- lib/components/tiptap/image/ImageResizeComponent.tsx
+++ lib/components/tiptap/image/ImageResizeComponent.tsx
@@ -1,27 +1,29 @@
 import React, { useState, useCallback, useEffect, useRef } from 'react'
 import { NodeViewWrapper } from '@tiptap/react'
-import { Resize } from './resizeIcon'
+import { Resizable } from 're-resizable'
 import { LoadingPlaceholder } from './loadingPlaceholder'
+import { Resize } from './resizeIcon'
 
 export const ImageResizeComponent = (props: any) => {
   const [loading, setLoading] = useState(true)
-  const [aspectRatio, setAspectRatio] = useState(1)
-  const [proseMirrorContainerWidth, setProseMirrorContainerWidth] = useState(0)
-  const imageRef = useRef<HTMLImageElement | null>(null)
-  const resizeRef = useRef<{
-    active: boolean
-    startX: number
-    startWidth: number
-  }>({
-    active: false,
-    startX: 0,
-    startWidth: 0,
+  const [size, setSize] = useState({
+    width: props.node.attrs.width,
+    height: props.node.attrs.height,
   })
+  const [aspectRatio, setAspectRatio] = useState(1)
+  const [maxSize, setMaxSize] = useState({
+    maxWidth: 0,
+    maxHeight: 0,
+  })
+  const imageRef = useRef<HTMLImageElement | null>(null)
 
   useEffect(() => {
     const proseMirrorContainerDiv = document.querySelector('.ProseMirror')
     if (proseMirrorContainerDiv) {
-      setProseMirrorContainerWidth(proseMirrorContainerDiv.clientWidth)
+      setSize((prevSize) => ({
+        ...prevSize,
+        maxWidth: proseMirrorContainerDiv.clientWidth,
+      }))
     }
   }, [])
 
@@ -30,113 +32,92 @@ export const ImageResizeComponent = (props: any) => {
     if (imageRef.current) {
       const naturalWidth = imageRef.current.naturalWidth
       const naturalHeight = imageRef.current.naturalHeight
+      setMaxSize({
+        maxWidth: naturalWidth,
+        maxHeight: naturalHeight,
+      })
       setAspectRatio(naturalWidth / naturalHeight)
     }
   }, [])
 
-  const limitMinSize = (width: number, height: number) => {
-    const minSize = 200
-    return {
-      width: Math.max(width, minSize),
-      height: Math.max(height, minSize),
-    }
+  const onResize = (_e: any, _direction: any, ref: any, _d: any) => {
+    const newWidth = ref.style.width
+    const newHeight = ref.style.height
+    setSize({
+      width: parseFloat(newWidth),
+      height: parseFloat(newHeight),
+    })
   }
 
-  const updateSize = useCallback(
-    (newWidth: number) => {
-      const newHeight = newWidth / aspectRatio
-      const { width, height } = limitMinSize(newWidth, newHeight)
-
-      try {
-        if (props.node && props.node.type.name === 'uploadImage') {
-          props.updateAttributes({ width, height })
-        } else {
-          console.warn('Node type mismatch or node not found')
-        }
-      } catch (error) {
-        console.error('Error updating image size:', error)
-      }
-    },
-    [aspectRatio, props]
-  )
-
-  const handleResizeStart = useCallback(
-    (event: React.PointerEvent, direction: 'left' | 'right') => {
-      event.preventDefault()
-      event.stopPropagation()
-
-      if (imageRef.current) {
-        resizeRef.current = {
-          active: true,
-          startX: event.clientX,
-          startWidth: imageRef.current.clientWidth,
-        }
-
-        const handleResizeMove = (moveEvent: PointerEvent) => {
-          if (!resizeRef.current.active) return
-
-          moveEvent.preventDefault()
-          moveEvent.stopPropagation()
-
-          const diffX = moveEvent.clientX - resizeRef.current.startX
-          const newWidth =
-            direction === 'right'
-              ? resizeRef.current.startWidth + diffX
-              : resizeRef.current.startWidth - diffX
-
-          updateSize(
-            Math.min(Math.max(newWidth, 200), proseMirrorContainerWidth)
-          )
-        }
-
-        const handleResizeEnd = () => {
-          resizeRef.current.active = false
-          window.removeEventListener('pointermove', handleResizeMove)
-          window.removeEventListener('pointerup', handleResizeEnd)
-          window.removeEventListener('pointercancel', handleResizeEnd)
-        }
-
-        window.addEventListener('pointermove', handleResizeMove, {
-          passive: false,
-        })
-        window.addEventListener('pointerup', handleResizeEnd)
-        window.addEventListener('pointercancel', handleResizeEnd)
-      }
-    },
-    [proseMirrorContainerWidth, updateSize]
-  )
+  const onResizeStop = (_e: any, _direction: any, ref: any, _d: any) => {
+    const newWidth = ref.style.width
+    const newHeight = ref.style.height
+    console.log(newWidth, newHeight)
+    props.updateAttributes({
+      width: parseFloat(newWidth),
+      height: parseFloat(newHeight),
+    })
+  }
 
   return (
     <NodeViewWrapper
       className='image-resizer'
       style={{
         outline: props.selected ? '3px solid #0C41BB' : 'none',
-        borderColor: props.selected ? '#625df580' : 'none',
         borderRadius: '5px',
       }}
     >
       {loading && <LoadingPlaceholder />}
       <div style={{ display: loading ? 'none' : 'block' }}>
-        <div
-          className='resize-trigger left'
-          onPointerDown={(e) => handleResizeStart(e, 'left')}
-          style={{ touchAction: 'none' }}
+        <Resizable
+          size={size}
+          onResize={onResize}
+          onResizeStop={onResizeStop}
+          lockAspectRatio={aspectRatio}
+          maxWidth={maxSize.maxWidth}
+          maxHeight={maxSize.maxHeight}
+          enable={{
+            top: false,
+            right: true,
+            bottom: false,
+            left: true,
+            topRight: false,
+            bottomRight: false,
+            bottomLeft: false,
+            topLeft: false,
+          }}
+          handleComponent={{
+            left: (
+              <div
+                className='resize-trigger left'
+                style={{ cursor: 'ew-resize', width: '10px', height: '25%' }}
+              >
+                <Resize />
+              </div>
+            ),
+            right: (
+              <div
+                className='resize-trigger right'
+                style={{ cursor: 'ew-resize', width: '10px', height: '25%' }}
+              >
+                <Resize />
+              </div>
+            ),
+          }}
         >
-          <Resize />
-        </div>
-        <div
-          className='resize-trigger right'
-          onPointerDown={(e) => handleResizeStart(e, 'right')}
-          style={{ touchAction: 'none' }}
-        >
-          <Resize />
-        </div>
-        <img
-          {...props.node.attrs}
-          ref={imageRef}
-          className='postimage'
-          onLoad={handleImageLoad}
-        />
+          <img
+            {...props.node.attrs}
+            ref={imageRef}
+            className='postimage'
+            onLoad={handleImageLoad}
+            style={{
+              width: '100%',
+              height: '100%',
+              objectFit: 'contain',
+              borderRadius: '5px',
+            }}
+          />
+        </Resizable>
       </div>
     </NodeViewWrapper>
   )
